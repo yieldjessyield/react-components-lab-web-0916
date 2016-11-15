4792e261e547599e84b7c6dafc19353a531c4bd2
Jess <jess.harrelson0707@gmail.com>
2016-11-15 16:33:51 -0500

    Done.

--- index.js
+++ index.js
@@ -5,6 +5,69 @@ const BUTCHER_PRODUCTS = [
   'Ribeye'
 ];
 
+
+
+// const OlderCoaster = React.createClass({
+//   render() {
+
+//     return React.createElement('div', {}, [
+//       React.createElement('p', {}, 'Two grannies having the time of their life!'),
+//       React.createElement('p', {}, 'Passengers:'),
+//       React.createElement('ul', {}, [
+//         React.createElement('li', {}, 'Agnes'),
+//         React.createElement('li', {}, 'Muriel')
+//         ]),
+//     ]);
+//   }
+// });
+
+const OlderCoaster = React.createClass({
+ render(){
+     let granny = React.createElement('p',{}, 'Two grannies having the time of their life!');
+     let li = React.createElement('li', {}, 'Agnes');
+     let otherLi = React.createElement('li', {}, 'Muriel');
+     let ul = React.createElement('ul',{}, [li, otherLi]);
+     let passengers = React.createElement('p',{},'Passengers:')
+     return  React.createElement('div',{className:'oldercoaster'}, [granny, passengers, ul]);
+ }
+});
+
+
+class InFrontOfYou extends React.Component {
+  render() {
+    return React.createElement('div', {}, [
+      React.createElement('p', {}, "You shouldn't look too far."),
+      React.createElement('p', {}, ["Sometimes, the solution is ",
+        React.createElement('strong', {}, "right in front of you.")
+        ])
+      ]);
+  }
+}
+
+class ButcherShop extends React.Component{
+  render(){
+    let prods = BUTCHER_PRODUCTS.map(function(item){
+      return React.createElement('li', {}, item)
+    })
+
+    return React.createElement('div', {className: "butcher-shop"}, [
+      React.createElement('p', {}, "Hello! We have the following products for sale today:"),
+      React.createElement('ul', {}, prods)
+      ])
+
+  }
+}
+
+// <div class="butcher-shop">
+//   <p>Hello! We have the following products for sale today:</p>
+//   <ul>
+//     <li>Tenderloin</li>
+//     <li>Short ribs</li>
+//     <li>Beef shin</li>
+//     <li>Ribeye</li>
+//   </ul>
+// </div>
+
 ReactDOM.render(
   React.createElement('div', {}, [
     React.createElement(OlderCoaster),
@@ -13,3 +76,5 @@ ReactDOM.render(
   ]),
   document.getElementById('main')
 );
+
+
